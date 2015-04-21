338da45f6fc17f22a44beafca3a77d3cc7613750
Dan Eder <daneder@gmail.com>
2015-04-20 21:48:23 -0400

    Create alphabetical_string.rb

---
+++ alphabetical_string.rb
@@ -0,0 +1,11 @@
+#takes in a string and returns the string sorted alphabetically
+#from coderbyte.com
+
+def AlphabetSoup(str)
+
+  str = str.split('')
+  str = str.sort.to_s
+  return str 
+         
+end
+   
