9fa44757c3236133bcffaf07be8f15601535c1e9
Dan Eder <daneder@gmail.com>
2015-04-29 20:18:10 -0400

    Update iteration_drills.rb

--- iteration_drills.rb
+++ iteration_drills.rb
@@ -1,16 +1,77 @@
-#fun with iteration
+#Write Ruby<->English answers here as comments in your code
+
+#.each into English
+
+# 1) for every element called 'number' in the array 'numbers,'
+#print out the product of 3 times that number
+
+#2) for every element called 'name' in the array called 'names'
+# print out the length of the element
+
+#3) for every element 'number' in the array called 'numbers',
+# add that number to the variable sum
+
+#4) for every key/value pairing in the hash 'hash',
+# print out "#{key} is #{value} years old."
+
+#5) for every key/value pairing in the has 'account',
+# add the value element to the variable sum, then print out the total sum
+
+#6) for every key/value pairing in the addresses hash, print out
+# "#{key} lives on #{value}"
+
+#English into .each
+
+# 1) sentences.each do |word|
+#   puts word
+# end
+#
+# 2)
+# ma_area_code = [339, 351, 413, 508, 617, 774, 781, 857, 978]
+# numbers.each do |phone_number|
+#   if ma_area_code.include? phone_number
+#     puts phone_number
+#   end
+# end
+#
+# 3)
+# numbers.each do |number|
+#   if number.odd?
+#     puts number
+#   end
+# end
+# 4)
+# ages.each do |name, age|
+#   puts "#{name} is #{age} years old."
+# end
+#5)
+# ages.each do |name, age|
+#   if age > 10
+#     puts "#{name}, #{age}."
+#   end
+# end
+#6)
+# ages.each do |name, age|
+#   if age.even?
+#     puts name, age
+#   end
+# end
 
 array = [28214, 63061, 49928, 98565, 31769, 42316, 23674, 3540, 34953, 70282, 22077, 94710, 50353, 17107, 73683, 33287, 44575, 83602, 33350, 46583]
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * What is the sum of all the numbers in `array`?
-puts "Sum of all numbers in 'array' is: #{array.inject(:+)}"
+sum = 0
+array.each { |element| sum += element }
+puts "The sum of all numbers in the array is: " + sum.to_s
 
 # * How would you print out each value of the array?
-array.each {|element| print element.to_s + " "}
+puts "Here is each value of the array: "
+puts array.each { |element| puts element.to_i }
 
 # * What is the sum of all of the even numbers?
+
 sum = 0
 array.each { |element| sum += element if element.even? }
 puts "Sum of even numbers is: " + sum.to_s
@@ -30,21 +91,24 @@ sum = 0
 array.each { |element| sum += (element * element) }
 puts "Sum of all squares in the array is: " + sum.to_s
 
-puts array = ["joanie", "annamarie", "muriel", "drew", "reva", "belle", "amari", "aida", "kaylie", "monserrate", "jovan", "elian", "stuart", "maximo", "dennis", "zakary", "louvenia", "lew", "crawford", "caitlyn"]
+array = ["joanie", "annamarie", "muriel", "drew", "reva", "belle", "amari", "aida", "kaylie", "monserrate", "jovan", "elian", "stuart", "maximo", "dennis", "zakary", "louvenia", "lew", "crawford", "caitlyn"]
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * How would you print out each name backwards in `array`?
 puts "Each name in array backwards: "
-array.each do |element| print element.reverse + " " end
+array.each { |element| print element.reverse } + " " end
 
 # * What are the total number of characters in the names in `array`?
 sum = 0
-array.each do |element| sum += element.length end
+array.each { |element| sum += element.length }
+
 puts "\nThere are #{sum} characters in the names in array."
 
+
 # * How many names in `array` are less than 5 characters long?
-puts "#{array.count{|x| x.length < 5}} names in array are less than 5 characters long."
+puts "#{array.count{ |x| x.length < 5 }} names in array are less than 5 characters long."
+
 
 # * How many names in `array` end in a vowel?
 sum = 0
@@ -53,6 +117,7 @@ array.each do |element|
     sum += 1
   end
 end
+
 puts "#{sum} names in array end in a vowel."
 
 # * How many names in `array` are more than 5 characters long?
@@ -62,6 +127,7 @@ array.each do |element|
     sum += 1
   end
 end
+
 puts "#{sum} characters in array are more than 5 characters long."
 
 # * How many names in `array` are exactly 5 characters in length?
@@ -71,6 +137,7 @@ array.each do |element|
     sum += 1
   end
 end
+
 puts "#{sum} names in array are exactly 5 characters long."
 
 best_records = {
@@ -84,63 +151,72 @@ best_records = {
  "Nirvana"=>"Nevermind"
  }
 
- best_records = {
-  "Tupac"=>"All Eyez on Me",
-  "Eminem"=>"The Marshall Mathers LP",
-  "Wu-Tang Clan"=>"Enter the Wu-Tang (36 Chambers)",
-  "Led Zeppelin"=>"Physical Graffiti",
-  "Metallica"=>"The Black Album",
-  "Pink Floyd"=>"The Dark Side of the Moon",
-  "Pearl Jam"=>"Ten",
-  "Nirvana"=>"Nevermind"
-  }
- puts "There are #{best_records.length} records in best_records.\n \n"
+#  Write Ruby code to find out the answers to the following questions:
 
- puts "The artists listed are: #{best_records.keys}\n \n"
+# * How would you print out all the names of the artists?
 
- puts "The album names are: #{best_records.values}\n \n"
+puts "The artists listed are: #{best_records.keys}\n\n"
 
- best_records.delete "Eminem"
- puts "The Eminem key-value pair has been deleted from the list: #{best_records}\n \n"
 
- best_records["Vampire Weekend"] = ["Vampire Weekend"]
- puts "Favorite musician and their best album has been added to the list: #{best_records}\n \n"
+# * How would you print out all the names of the albums?
 
- best_records["Nirvana"] = ["Bleach"]
- puts "Nirvana's album has been changed to another: #{best_records}\n \n"
+puts "The album names are: #{best_records.values}\n\n"
 
- puts "Q: Is Nirvana included in best_records? A: #{best_records.has_key?("Nirvana")}\n \n"
+# * Which artist has the longest name?
 
- puts "Q: Is Soundgarden included in best_records? A: #{best_records.has_key?("Soundgarden")}\n \n"
+# * How would you change all the album titles for every artist to `Greatest Hits`?
 
- best_records["Soundgarden"] = ["Superunknown"]
- puts "Key-value pair added for Soundgarden: #{best_records}\n \n"
+# * How would you delete a key-value pair if the artist's name ends in a vowel?
 
- puts "Q: Which key-value pairs have a key that has a length less than or equal to 6 characters?"
-   best_records.each do |key, value|
-     if key.length <= 6
-       puts key, value
-       puts
-     end
-   end
+best_records.each do |key, value|
+  new_key = key.split("")
+  if new_key[-1].match(/[aeiou]/)
+    best_records.delete(key)
+  end
+end
 
- puts "Q: Which key-value pairs have a value that is greater than 10 characters?"
-   best_records.each do |key,value|
-     if value.length > 10
-       puts key, value
-       puts
-     end
-   end
+puts "All artists whose names end in a vowel eliminated: #{best_records.to_s}"
+
+
 
 ages = {"Arch"=>89, "Gretchen"=>93, "Simone"=>12, "Daija"=>96, "Alivia"=>22, "Serena"=>28, "Alek"=>3, "Lula"=>24, "Christian"=>62, "Darryl"=>47, "Otha"=>32, "Evalyn"=>44, "Lincoln"=>27, "Rebeca"=>99, "Beatrice"=>99, "Kelton"=>10, "Zachary"=>18, "Aurelie"=>91, "Dell"=>71, "Lisandro"=>22}
 
 # Write Ruby code to find out the answers to the following questions:
 
 # * How would you print out all the names of `ages`?
+puts "All the names of 'ages': #{ages.keys}.\n"
+
 # * How would you print out each key-value pair in the format of `<name> is <age> years old.`?
+ages.each { |key, value| puts "#{key} is #{value} years old." }
+
 # * How would you print out every person with odd numbered age?
+
+puts "\nEvery person with an odd-numbered age: "
+
+ages.each do |key, value|
+  unless value % 2 == 0
+    puts key
+  end
+end
+
 # * How would you delete everyone under 25 years of age?
+puts "\nEveryone over 25 years of age: "
+
+older_ages = Hash.new
+
+ages.each do |key,value|
+  if value > 25
+    older_ages[key] = value
+  end
+end
+puts older_ages.to_s
+
 # * What is the name and age of everyone with a name greater than or equal to 5 characters?
+
+puts "Everyone with a name greater than or equal to 5 characters: "
+
+ages.each { |key,value| puts key, value if key.length >= 5 }
+
 
 people =
 {
@@ -185,50 +261,14 @@ people =
 
 # Write Ruby code to find out the answers to the following questions:
 
-# How many people are in people (excluding children)?
-puts "There are #{people.keys.length} people in the hash."
-puts
-# What are the names of all the people?
-puts "The names of all five people are: "
-puts people.keys
-puts
-# What is the phone number of Alia O'Conner PhD?
-puts "The phone number of Alia O'Conner PhD is: " + people["Dr. Adela DuBuque"]["phone"].to_s
-puts
-# How many children does Brian Heller have?
-puts "Brian Heller has #{people["Brian Heller"]["children"].count.to_s} child."
-# What company does Dr. Adela DuBuque work for?
-puts
-puts "Dr. Adela DuBuque works for #{people["Dr. Adela DuBuque"]["company"]}."
-puts
-# What are the names of the people who have children?
-puts "The people with children are: "
-people.each do |key, value|
-  if value.has_key?("children")
-    puts key
-  end
-end
-puts
+# * How would you print out all the names of `people`?
 
-# What are the names of the people who do not have children?
-puts "The people without children are: "
- people.each do |key, value|
-   unless value.has_key?("children")
-     puts key
-   end
-  end
 
-# What is Brian Heller's child's name?
-puts "Brian Heller's child's name is #{people["Brian Heller"]["children"]}."
-puts
+# * How would you print out all the names of `people` and which company they work for?
+# * What are the names of all the children of everyone in `people`?
+# * What are the names of all the companies that people work for?
+# * How would you convert all the phone numbers to the same standard (pick one)?
 
-# What is Maryse Johns' first child's name?
-puts "Maryse Johns' child's first name is #{people["Maryse Johns"]["children"].first.to_s}."
-
-# Dr. Adela DuBuque just had a baby named Tomas. How would you update the hash for this information?
-puts
-people["Dr. Adela DuBuque"]["children"] = "Tomas"
-puts "Dr. Adela DuBuque's new child is: " + people["Dr. Adela DuBuque"]["children"].to_s
 
 people =
 [
@@ -289,7 +329,9 @@ people =
 ]
 
 # Write Ruby code to find out the answers to the following questions:
+
 # * What are the names of everyone in `people`?
+
 0.upto(2){ |i| puts people[i].keys }
 
 # * What are the names of all the children in `people`?
@@ -302,25 +344,6 @@ people.each do |element|
   end
 end
 
-
-# * How would you create a new hash called `phone_numbers` that has a key of a 
-# * name and value of a phone number in `people`?
-
-people[0].merge!("phone_numbers" => {"name" => "phone number"})
-puts people[0].to_s
-puts
-
-# * How would you create a new hash called `employers` that has a key of a name
-# * and a value of a company name in `people`?
-
-people[0].merge!("employers" => {"name" => "company name"})
-puts people[0].to_s
-puts
-
-# * How would you create a new hash called `children_count` that has a key of a
-# * name and a value of the number of children a person has?
-
-number = people[0]["Derek Wehner"]["children"].count
-people[0].merge!("children_count" => {"Derek Wehner" => number})
-
-puts people[0]["Derek Wehner"]["children_count"].to_s
+# * How would you create a new hash called `phone_numbers` that has a key of a name and value of a phone number in `people`?
+# * How would you create a new hash called `employers` that has a key of a name and a value of a company name in `people`?
+# * How would you create a new hash called `children_count` that has a key of a name and a value of the number of children a person has?
